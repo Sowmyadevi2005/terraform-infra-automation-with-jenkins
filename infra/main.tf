0cf867e8b717dc900978c67703fe1f40d265c6a3
Soowmyadevi Telidevara <sowmyadevi.telidevara2005@gmail.com>
2025-06-23 11:54:01 +0530

    Update main.tf
    changed public key path

--- infra/main.tf
+++ infra/main.tf
@@ -30,7 +30,7 @@ module "ec2" {
   ami_id                     = var.ec2_ami_id
   instance_type              = "t2.micro"
   tag_name                   = "Ubuntu Linux EC2"
-  public_key                 = file("C:/Users/Sowmya/.ssh/id_rsa.pub")
+  public_key                 = file("${path.module}/keys/id_rsa.pub")
   subnet_id                  = tolist(module.networking.public_subnets)[0]
   sg_enable_ssh_https        = module.security_group.sg_ec2_sg_ssh_http_id
   ec2_sg_name_for_python_api = module.security_group.sg_ec2_for_python_api
